c8fffb04afa55dfa0f409f101efa5e07fa59ff9a
Ryan Williams <r.d.williams.ca@gmail.com>
2024-07-19 13:48:06 -0700

    car details modal stucture complete, next to implement car images api

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -46,7 +46,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <DialogPanel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5">
+                <DialogPanel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
                   <button
                     type="button"
                     onClick={closeModal}
@@ -107,6 +107,20 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     <h2 className="font-semibold text-xl capitalize">
                       {car.make} {car.model}
                     </h2>
+
+                    <div className="mt-3 flex flex-wrap gap-4">
+                      {Object.entries(car).map(([key, value]) => (
+                        <div
+                          className="flex justify-between gap-5 w-full text-right"
+                          key={key.split("_").join(" ")}
+                        >
+                          <h4 className="text-gray capitalize">{key}</h4>
+                          <p className="text-black-100 font-semibold capitalize">
+                            {value}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </DialogPanel>
               </TransitionChild>
